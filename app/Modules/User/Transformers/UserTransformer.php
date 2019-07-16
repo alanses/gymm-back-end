5cbd69af82c15302cb6090ee4ce9f3f14eca658f
yaroslav <yaroslav.semeniv@redentu.com>
2019-07-16 13:16:13 +0300

    add user type

--- app/Modules/User/Transformers/UserTransformer.php
+++ app/Modules/User/Transformers/UserTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\User\Transformers;
 
+use App\Modules\User\Entities\User;
 use App\Ship\Abstraction\AbstractEntity;
 use App\Ship\Interfaces\EntityInterface;
 use App\Ship\Parents\Transformer;
@@ -32,13 +33,29 @@ class UserTransformer extends Transformer
     {
         $response = [
             'user_id' => $entity->id,
+            'user_type' => $this->getUserType($entity),
             'email' => $entity->email,
             'name' => $entity->name,
-            'response-content' => $entity->response_content,
+            'content' => $entity->response_content,
             'created_at' => $entity->created_at,
             'updated_at' => $entity->updated_at
         ];
 
         return $response;
+    }
+
+    private function getUserType(AbstractEntity $entity)
+    {
+        if($entity->user_type == User::$is_user) {
+            return 'user';
+        }
+
+        if($entity->user_type == User::$is_gym) {
+            return 'gym';
+        }
+
+        if($entity->user_type == User::$is_supper_admin) {
+            return 'supper_admin';
+        }
     }
 }
