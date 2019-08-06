362ae78990b7d6be5d3a89cc5e9449125194c2d0
yaroslav <yaroslav.semeniv@redentu.com>
2019-08-06 15:15:45 +0300

    init booking module

---
+++ app/Modules/Booking/Http/Controllers/BookingControllerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Modules\Booking\Http\Controllers;
+
+use App\Ship\Parents\ApiController;
+
+class BookingController extends ApiController
+{
+    public function getAllBookingControllers()
+    {
+
+    }
+
+    public function getBookingControllerById()
+    {
+
+    }
+
+    public function createBookingController()
+    {
+
+    }
+
+    public function updateBookingController()
+    {
+
+    }
+
+    public function deleteBookingController()
+    {
+
+    }
+}
