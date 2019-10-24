d7389d1cd9cef72cfe40e776100cd40449980b5b
yaroslav.semeniv@redentu.com <alansis98235>
2019-10-24 17:58:50 +0300

    test confirm payment

---
+++ app/Modules/Payment/Http/Controllers/PaymentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Modules\Payment\Http\Controllers;
+
+use App\Ship\Parents\ApiController;
+use Illuminate\Support\Facades\Log;
+
+class PaymentController extends ApiController
+{
+    public function testPayment()
+    {
+        return view('paymenttest');
+    }
+
+    public function testPaymentConfirm()
+    {
+        dd(request());
+        Log::info('test', request()->all());
+    }
+}
