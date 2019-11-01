2161bd604d684641a66223b50fa2ecefddc50cb6
yaroslav.semeniv@redentu.com <alansis98235>
2019-11-01 18:04:32 +0200

    fix

---
+++ app/Modules/Payment/Actions/ConfirmSubscribeAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Payment\Actions;
+
+use App\Modules\Payment\Tasks\CheckIfTransactionRejectedTask;
+use App\Modules\Payment\Tasks\ConfirmPaymentTask;
+use App\Modules\Payment\Tasks\MakeSubscribeTask;
+use App\Ship\Abstraction\AbstractAction;
+use Illuminate\Http\Request;
+use stdClass;
+
+class ConfirmSubscribeAction extends AbstractAction
+{
+    public function run(Request $request) :stdClass
+    {
+        $payment = $this->call(ConfirmPaymentTask::class, [$request]);
+
+        $this->call(CheckIfTransactionRejectedTask::class, [$payment]);
+
+        $subscribe = $this->call(MakeSubscribeTask::class, [$payment]);
+
+        return $subscribe;
+    }
+}
---
+++ app/Modules/Payment/Actions/MakeSubscribeAction.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Modules\Payment\Actions;
+
+use App\Modules\Payment\Http\Requests\SubscribeRequest;
+use App\Modules\Payment\Tasks\CheckIfNeed3DVerificationTask;
+use App\Modules\Payment\Tasks\CheckIfPaymentConfirmTask;
+use App\Modules\Payment\Tasks\CheckIfTransactionRejectedTask;
+use App\Modules\Payment\Tasks\MakeSubscribeTask;
+use App\Modules\Payment\Tasks\SendPayment;
+use App\Modules\Plans\Tasks\GetPlanTask;
+use App\Modules\Plans\Tasks\SubscribeUserToPlanTask;
+use App\Modules\Transactions\Tasks\RegisterTransactionTask;
+use App\Modules\User\Tasks\GetAuthenticatedUserTask;
+use App\Ship\Abstraction\AbstractAction;
+
+class MakeSubscribeAction extends AbstractAction
+{
+    public function run(SubscribeRequest $request)
+    {
+        $user = $this->call(GetAuthenticatedUserTask::class);
+
+        $plan = $this->call(GetPlanTask::class, [], [
+            ['findByField' => ['id', $this->getPlanId($request)]]
+        ]);
+
+        $cryptID = $this->getCardCryptogramPacket($request);
+
+        $payment = $this->call(SendPayment::class, [], [
+            ['makeSubscribe' => [$plan, $cryptID, $user]]
+        ]);
+
+        $this->call(CheckIfTransactionRejectedTask::class, [$payment]);
+
+        $this->call(CheckIfNeed3DVerificationTask::class, [$payment]);
+
+        $this->call(MakeSubscribeTask::class, [$payment]);
+
+        $this->call(SubscribeUserToPlanTask::class, [$plan], [
+            ['whereUserIdIs' => [$user->id]]
+        ]);
+
+        $this->call(RegisterTransactionTask::class, [$plan, $user], [
+            ['addPoints' => [$user, $plan]],
+            ['setOperationType' => ['add']]
+        ]);
+
+        return $payment;
+    }
+
+    private function getCardCryptogramPacket(SubscribeRequest $request)
+    {
+        return $request->CardCryptogramPacket;
+    }
+
+    private function getPlanId(SubscribeRequest $request)
+    {
+        return $request->plan_id;
+    }
+}
