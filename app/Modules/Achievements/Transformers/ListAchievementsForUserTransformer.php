46b2372ad47cbef8197292a8447568c1446f6371
andrew4worku <andrew4worku@gmail.com>
2019-12-16 11:54:07 +0200

    make return list achievements

--- app/Modules/Achievements/Transformers/ListAchievementsForUserTransformer.php
+++ app/Modules/Achievements/Transformers/ListAchievementsForUserTransformer.php
@@ -2,7 +2,9 @@
 
 namespace App\Modules\Achievements\Transformers;
 
+use App\Modules\Achievements\Entities\Achievement;
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Facades\Storage;
 
 class ListAchievementsForUserTransformer extends Resource
 {
@@ -14,7 +16,44 @@ class ListAchievementsForUserTransformer extends Resource
      */
     public function toArray($request)
     {
-        dd($this);
-        return parent::toArray($request);
+        return [
+            'achievements' => $this->getAchievements()
+        ];
+    }
+
+    private function getAchievements()
+    {
+        return $this->resource['achievements']->map(function ($achievement) {
+            return [
+                'id' => $achievement->id,
+                'displayed_name' => $achievement->displayed_name,
+                'count_classes' => $achievement->count_classes,
+                'activity_id' => $achievement->activity_id,
+                'image' => $this->getImage($achievement),
+                'visited_by_user' => $this->getVisitedByUser($achievement)
+            ];
+        });
+    }
+
+    private function getVisitedByUser(Achievement $achievement) {
+        if($userPassedBookings = $this->resource['user']->userPassedBookings) {
+            $countVisit = 0;
+            foreach ($userPassedBookings as $userPassedBooking) {
+                if($classSchedule = $userPassedBooking->classSchedule) {
+                    if($achievement->activity_id == $classSchedule->activities_id) {
+                        $countVisit++;
+                    }
+                }
+            }
+        }
+
+        return $countVisit;
+    }
+
+    private function getImage(Achievement $achievement)
+    {
+        if($image = $achievement->image) {
+            return env('APP_URL') . Storage::url($image);
+        }
     }
 }
