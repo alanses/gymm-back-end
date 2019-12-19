de76bb7102ecf2e45b37fa4d669ceea39099d68f
andrew4worku <andrew4worku@gmail.com>
2019-12-19 13:27:58 +0200

    add messages on validation

--- app/Modules/Achievements/Http/Requests/AchievementRequest.php
+++ app/Modules/Achievements/Http/Requests/AchievementRequest.php
@@ -22,7 +22,19 @@ class AchievementRequest extends AbstractRequest
             'kz_displayed_name' => 'required|string|max:255',
             'activity_id' => 'required|integer|exists:activities,id',
             'count_classes' => 'required|integer',
-            'image' => 'nullable|file'
+            'image' => 'nullable|file|mimes:jpeg,bmp,png,svg'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'displayed_name.required' => 'Field Achievement name is required',
+            'displayed_name.max' => 'Field Achievement name has max symbols 255',
+            'image.file' => 'Field Image must by file',
+            'image.mimes' => 'Field Image must by types: jpeg,bmp,png,svg',
+            'kz_displayed_name.max' => 'Field Achievement kz name has max symbols 255',
+            'ru_displayed_name.max' => 'Field Achievement ru name has max symbols 255'
         ];
     }
 
